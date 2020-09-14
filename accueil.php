0ac6e1ad39885275d8e40c02d5a4c920959781b7
Baptiste <baptiste.lecat44@gmail.com>
2020-09-14 22:22:07 +0200

    Initial commit

---
+++ accueil.php
@@ -0,0 +1,27 @@
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="accueil.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <title>Integration | Accueil</title>
+  </head>
+  <body>
+    <header>
+      <h1><span>&lt/Int</span>Egration/&gt</h1>
+    </header>
+
+    <main>
+      <div class="clock_container">
+        <h2>22j 15h 12m 26s</h2>
+        <hr>
+      </div>
+      <div class="form_container">
+        <p>Inscrit toi pour participer à la journée d'intégration. Le jour venu tu auras accès aux différentes activités.</p>
+        <button type="button" name="buttonInscription">Inscription</button>
+      </div>
+      <div class="footer">
+        <p>Vous avez déjà un compte? <a href="#">Connectez-vous.</a></p>
+      </div>
+    </main>
+  </body>
+</html>
