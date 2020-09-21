e9c73694c31a5a634040286e73b3d070b3c84a0d
BaptisteLecat <60200125+BaptisteLecat@users.noreply.github.com>
2020-09-21 10:05:07 +0200

    Icon LeaderBoard

--- views/leaderBoard.php
+++ views/leaderBoard.php
@@ -22,10 +22,12 @@ if ($liste_load["success"] == 1) {
     foreach ($uneliste_nomEtudiant as $key2 => $value2) {
       $stringEtudiant .= $value2.', ';
     }
+    $stringEtudiantLenght = strlen($stringEtudiant);
+    $stringEtudiant =  substr($stringEtudiant, 0, $stringEtudiantLenght - 2);
     if ($key % 2 == 1 && $key >= 3) {
       $html .= '<div class="container_equipe" style="background-color: #253644;">
           <div class="container_icon">
-
+            <img src="assets\icons\leaderboard_icon.png">
           </div>
           <div class="container_information">
             <h2>'.$value->GET_Name().'</h2>
@@ -40,7 +42,7 @@ if ($liste_load["success"] == 1) {
         case 2:
         $html .= '<div class="container_equipe" style="background-color: #B5242E;">
             <div class="container_icon">
-
+            <img src="assets\icons\leaderboard_third.png">
             </div>
             <div class="container_information">
               <h2>'.$value->GET_Name().'</h2>
@@ -54,7 +56,7 @@ if ($liste_load["success"] == 1) {
         case 1:
         $html .= '<div class="container_equipe" style="background-color: #D62E3A;">
             <div class="container_icon">
-
+              <img src="assets\icons\leaderboard_second.png">
             </div>
             <div class="container_information">
               <h2>'.$value->GET_Name().'</h2>
@@ -68,7 +70,7 @@ if ($liste_load["success"] == 1) {
         case 0:
         $html .= '<div class="container_equipe" style="background-color: #F03A47; border-top-right-radius: 8px; border-top-left-radius: 8px;">
             <div class="container_icon">
-
+              <img src="assets\icons\leaderboard_first.png">
             </div>
             <div class="container_information">
               <h2>'.$value->GET_Name().'</h2>
